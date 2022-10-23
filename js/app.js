d815a8147a83ef8ee504ee36d757e8517dda68b1
f-ast1k <63163819+f-ast1k@users.noreply.github.com>
2022-10-24 00:11:08 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
 	//при клике на основную кнопку отправляем данные в строковом виде
 });
 
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send"); 
+	//при клике на основную кнопку отправляем данные в строковом виде
+});
+
 function showalert(){
 	window.alert("test2");
 }
