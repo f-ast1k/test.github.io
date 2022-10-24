728f24b9246a076e15b7aabdfd4f0a66815e8b13
f-ast1k <63163819+f-ast1k@users.noreply.github.com>
2022-10-24 23:37:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function input(){
+    var input_data = document.getElementById("1")
+    var value_data = Number(input_data.value)
+    if (value_data < 0) {
+        input_data.value = 0
+    }
+}
+
+function plus(){
+    var input_data = document.getElementById("1");
+    var value_data = Number(input_data.value);
+    input_data.value = value_data + 1;
+};
+
+function minus(){
+    var input_data = document.getElementById("1");
+    var value_data = Number(input_data.value);
+    if (value_data > 0) {
+        input_data.value = value_data - 1
+    }
+    else {
+        input_data.value = 0
+    }
+}
+
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.show();
