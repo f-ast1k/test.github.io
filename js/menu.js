ebb2dca9dc472ca8935ac2562e7c2fa9ad81fb7e
f-ast1k <63163819+f-ast1k@users.noreply.github.com>
2022-11-07 04:37:33 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -12,17 +12,65 @@ function clossesItem(item, className) {
     return null;
 }
 
+var category_section = document.querySelector(".content");
+var product_items = category_section.querySelectorAll(".product");
+var product = category_section.querySelector(".products");
 buttons.addEventListener("click", function(e) {
+
     var target = e.target;
     var button = clossesItem(target, "btn");
     if (button === null || button.classList.contains("is-active")) {
         return;
     }
-    e.preventDefault();
+    e.preventDefault(); 
+
+    var removeChildren = (item) => {
+        while (item.firstChild) {
+            item.removeChild(item.firstChild);
+        }
+    }
+
+    var updateChildren = (item, children) => {
+        // var child = document.querySelector(".product-not");
+        // category_section.removeChild();
+        removeChildren(item);
+        for (var i = 0; i < children.length; i += 1) {
+            item.appendChild(children[i]);
+        }
+    }  
+
     var filtarname = button.getAttribute("data-filter");
     var prevBtnActive = buttons.querySelector(".btn.is-active");
     prevBtnActive.classList.remove("is-active");
     button.classList.add("is-active")
+
+    var filterValue = button.getAttribute("data-filter");
+    if (filterValue === "0") {
+        updateChildren(product, product_items);
+        return;
+    }
+
+    var filteredItems = [];
+    for (var i = 0; i < product_items.length; i += 1) {
+        var current = product_items[i];
+        if (current.getAttribute("data-filter") === filterValue) {
+            filteredItems.push(current);
+        }
+    }
+    if (filteredItems.length === 0) {
+        if (document.querySelector(".product-not") === null) {
+            $(".products").before("<div class='product-not'>Товаров не найдено</div>");
+        }
+        updateChildren(product, filteredItems);
+        return;
+    }
+    if (category_section.querySelector(".product-not")) {
+        var not = category_section.querySelector(".product-not");
+        category_section.removeChild(not);
+    }
+    
+    updateChildren(product, filteredItems);
+
 });
 
 document.querySelector(".burger").addEventListener("click", function(e) {
@@ -37,7 +85,6 @@ document.querySelector(".burger").addEventListener("click", function(e) {
 
     $(".burger, .nav-menu").toggleClass("active-menu");
     $("body").toggleClass("openmenu");
-    console.log(e.target)
 });
 
 $(".content").click((e) => {
@@ -52,8 +99,8 @@ $(".content").click((e) => {
     }
 });
 
-$(".btn-more").click((e) => {
-    alert(document.querySelector(".btn-more").getAttribute("href"));
-    console.log(e.getAttribute);
-    // document.location.href = "https://www.cyberforum.ru/javascript/thread1888126.html";
-});
+// $(".btn-more").click((e) => {
+//     alert(document.querySelector(".btn-more").getAttribute("href"));
+//     console.log(e.getAttribute);
+//     // document.location.href = "https://www.cyberforum.ru/javascript/thread1888126.html";
+// });
